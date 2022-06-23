46922a656fc81ca8a23b865f4ab534e77fdcd073
Michael Strahl <michaelstrahl.sm@gmail.com>
2022-06-23 20:26:11 -0300

    Relacionamento um pra um

---
+++ 07-advanced-model/app/Http/Controllers/UserController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::find($id);
+
+        echo "<h1>Dados do Usuário</h1>";
+        echo "<p>Nome do usuário: $user->name </p>";
+        echo "<p>E-mail: $user->email</p><br>";
+
+        $userAddress = $user->addressDelivery()->get()->first();
+
+        if($userAddress):
+            echo "<h2>Endereço de entrega</h2>";
+            echo "<p>Endereço: $userAddress->address, $userAddress->number</p>";
+            echo "<p>Complemento: $userAddress->complement  CEP: $userAddress->zipcode</p>";
+            echo "<p>Cidade/Estado: $userAddress->city/$userAddress->state</p>";
+        endif;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
