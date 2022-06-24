a6ec4be2c306d5b7c15411a26d9c45ecc261e732
Michael Strahl <michaelstrahl.sm@gmail.com>
2022-06-24 16:28:28 -0300

    Consulta bruta RAW

--- 08-querybuilder-pagination/app/Http/Controllers/UserController.php
+++ 08-querybuilder-pagination/app/Http/Controllers/UserController.php
@@ -9,25 +9,49 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = DB::table('users')
-                ->select('users.id', 'users.name', 'users.status')
-                ->where('users.status', '1')
-                /**
-                 * Tipos de ordenação
-                 */
-                ->orderBy('name', 'DESC')
-                // ->oldest('name') // ASC
-                // ->latest('name') // ASC
-                // ->inRandomOrder()
+        // $users = DB::table('users')
+        //         ->select('users.id', 'users.name', 'users.status')
+        //         ->where('users.status', '1')
+        //         /**
+        //          * Tipos de ordenação
+        //          */
+        //         ->orderBy('name', 'DESC')
+        //         // ->oldest('name') // ASC
+        //         // ->latest('name') // ASC
+        //         // ->inRandomOrder()
 
-                ->limit(10)
-                ->offset(10)
-                ->get();
+        //         ->limit(10)
+        //         ->offset(10)
+        //         ->get();
+
+        // foreach ($users as $user) {
+        //     echo "#$user->id
+        //     Nome: $user->name
+        //     $user->status <br>";
+        // }
+
+        // $users = DB::table('users')
+        //         ->selectRaw('users.id, users.name, CASE WHEN users.status = 1 THEN "ATIVO" ELSE "INATIVO" END status_description')
+        //         ->whereRaw('(SELECT COUNT(1) FROM addresses a WHERE a.user = users.id) > 2')
+        //         ->whereRaw('users.status = 1')
+        //         ->orderByRaw('updated_at - created_at', 'ASC')
+        //         ->get();
+
+        $users = DB::select(DB::raw('
+                SELECT users.id, users.name,
+                CASE
+                    WHEN users.status = 1 THEN "ATIVO"
+                    ELSE "INATIVO" END status_description
+                FROM users
+                WHERE (SELECT COUNT(1) FROM addresses a WHERE a.user = users.id) > 2
+                    AND users.status = :userStatus
+                ORDER BY updated_at - created_at ASC
+        '), ['userStatus' => '1']);
 
         foreach ($users as $user) {
             echo "#$user->id
             Nome: $user->name
-            $user->status <br>";
+            $user->status_description <br>";
         }
     }
 }
