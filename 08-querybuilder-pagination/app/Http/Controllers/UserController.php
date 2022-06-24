828c6cecd79107fd00eff363890f32bf1273fbce
Michael Strahl <michaelstrahl.sm@gmail.com>
2022-06-24 16:02:11 -0300

    Construtor de query

--- 08-querybuilder-pagination/app/Http/Controllers/UserController.php
+++ 08-querybuilder-pagination/app/Http/Controllers/UserController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = DB::table('users')
+                ->select('users.id', 'users.name', 'users.status')
+                ->where('users.status', '1')
+                /**
+                 * Tipos de ordenação
+                 */
+                ->orderBy('name', 'DESC')
+                // ->oldest('name') // ASC
+                // ->latest('name') // ASC
+                // ->inRandomOrder()
+
+                ->limit(10)
+                ->offset(10)
+                ->get();
+
+        foreach ($users as $user) {
+            echo "#$user->id
+            Nome: $user->name
+            $user->status <br>";
+        }
+    }
 }
